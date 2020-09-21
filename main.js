9626b2b5193bb98d289ccc04e175aa0ec46efe9e
Faris Alamin <fma3210@gmail.com>
2020-09-21 07:36:37 +0100

    added image file and loaded it onto the canvas

--- main.js
+++ main.js
@@ -1,8 +1,36 @@
 //Prints line in console to make sure Pixi is working, with WebGL (can also be canvas)
 PIXI.utils.sayHello("WebGL");
 
-//Create a new instance of the Pixi Application object
-let app = new PIXI.Application({width: 512, height: 512, transparent: true});
+//Aliases
+let Application = PIXI.Application,
+    loader = PIXI.Loader.shared,
+    resources = PIXI.Loader.shared.resources,
+    Sprite = PIXI.Sprite;
 
-//Add the canvas (created with the #view method of the Application object) to the HTML body
+//Creating a new instance of the Pixi `Application` object
+let app = new Application({
+    width: 512,
+    height: 512,
+    antialias: true,
+    transparent: true,
+    resolution: 1
+  }
+);
+
+//Adding the canvas (created with the #view method of the Application object) to the HTML body
 document.body.appendChild(app.view);
+
+//Load an image and run the `setup` function when it's done
+loader
+  .add("images/cat.png")
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+
+  //Create the cat sprite
+  let cat = new Sprite(resources["images/cat.png"].texture);
+
+  //Add the cat to the stage
+  app.stage.addChild(cat);
+}
