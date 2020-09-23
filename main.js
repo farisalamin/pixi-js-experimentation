09e6019ea2839beda589e85e4be37d35217b6a0c
Faris Alamin <fma3210@gmail.com>
2020-09-23 18:21:19 +0100

    added collision border for sprite and added bouncing when border is hit

--- main.js
+++ main.js
@@ -34,6 +34,13 @@ function setup() {
   //Create the cat sprite
   cat = new Sprite(resources["images/cat.png"].texture);
 
+  //Set the cat's position
+  cat.position.set(0, 224);
+
+  //Set the cat's initial velocity
+  cat.vx = 3;
+  cat.vy = 3;
+
   //Add the cat to the stage
   app.stage.addChild(cat);
 
@@ -44,6 +51,62 @@ function setup() {
 //The gameLoop function which will contain anything I want to animate
 //Everything in here will happen once every frame
 const gameLoop = (delta) => {
-  cat.x += 1 + delta;
-  cat.y += 1 + delta;
+
+  //Create dimensions for container
+  const borderContainer = {x: 0, y: 0, width: app.renderer.width, height: app.renderer.height}
+
+  //Contain cat and store collision data
+  let catCollision = contain(cat, borderContainer);
+
+  bounce(cat, catCollision)
+
+  cat.x += cat.vx;
+  cat.y += cat.vy;
 };
+
+function bounce(sprite, collision) {
+
+  //If the sprite collides with the top or bottom, reverse the vertical velocity
+  if (collision.includes("top") ||
+      collision.includes("bottom")) {
+    sprite.vy *= -1;
+  };
+
+  //If the sprite collides with the left or right, reverse the horizontal velocity
+  if (collision.includes("left") ||
+      collision.includes("right")) {
+    sprite.vx *= -1;
+  };
+}
+
+function contain(sprite, container) {
+
+  let collision = [];
+
+  //Left
+  if (sprite.x < container.x) {
+    sprite.x = container.x;
+    collision.push("left");
+  }
+
+  //Top
+  if (sprite.y < container.y) {
+    sprite.y = container.y;
+    collision.push("top");
+  }
+
+  //Right
+  if (sprite.x + sprite.width > container.width) {
+    sprite.x = container.width - sprite.width;
+    collision.push("right");
+  }
+
+  //Bottom
+  if (sprite.y + sprite.height > container.height) {
+    sprite.y = container.height - sprite.height;
+    collision.push("bottom");
+  }
+
+  //Return the `collision` value
+  return collision;
+}
